fd0242ab408d2efeb9c9a20f8a8509d9b9d74c36
littljietu <littljietu@qq.com>
2015-09-23 09:53:11 +0800

    s

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -9,7 +9,7 @@
 
 <body>
 
-<?php $this->load->view('inc/header');?>
+<?php $this->load->view('inc/m_header');?>
 
 <div class="login_box" style="position:;">
 	<div class="login">
@@ -43,7 +43,7 @@
     </div>  
 </div>
 <br /><br /><br /><br /><br /><br />
-<?php $this->load->view('inc/footer');?>
+<?php $this->load->view('inc/m_footer');?>
 
 <?php echo _get_html_cssjs('js','jquery-1.11.3.min.js,jquery.form.js,page/common/header.js,page/common/show.js,page/user/login.js','js');?>
 
